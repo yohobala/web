a67022288f689de9a48d01f0355aa7d9f24caf18
huanghao <762100246@qq.com>
2021-05-05 18:05:22 +0800

    1

--- getCardPassword/views.py
+++ getCardPassword/views.py
@@ -7,14 +7,25 @@ import re
 #解压文件
 import rarfile
 
+from PIL import Image
+import pytesseract
+
 
 
 def index(request):
+
     print(request.method)
     if request.method == 'POST':
-        text = request.POST.get('cardText')
+        text = request.FILES.get('cardText')
+        print(text.name)
+        path = 'static/getCardPassword/' + text.name
+        with open(path, 'wb') as f:
+            for content in text.chunks():
+                f.write(content)
         print(text)
-        matchObj = re.search(r'[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}',text)
+        # image = Image.open('test.png')
+        # content = pytesseract.image_to_string(image, lang='chi_sim')  # 解析图片
+        matchObj = re.search(r'[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}',content)
         print(matchObj.group())
         return HttpResponse(matchObj.group())
 
